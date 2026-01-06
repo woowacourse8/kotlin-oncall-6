e7af30b01b39873a6b1356b369b45f932f0d1cb0
todays_sun_day <178suna@gmail.com>
2026-01-06 18:45:05 +0900

    feat: 잘못된 입력 시 재시도 로직 구현

---
+++ src/main/kotlin/oncall/Util.kt
@@ -0,0 +1,13 @@
+package oncall
+
+object Util {
+    fun <T> retryUntilValid(action: () -> T): T {
+        while (true) {
+            try {
+                return action()
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.")
+            }
+        }
+    }
+}
